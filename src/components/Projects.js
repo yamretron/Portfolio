879a013eaca0ddf6a6c0ac108ba64aeccf0c3b8a
unknown <yamretron@gmail.com>
2023-03-16 08:40:57 +0100

    Despliegue

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,12 +9,13 @@ export default function Projects() {
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Aplicaciones que he construido
+            Trabajos realizados
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            Estoy dedicado al Desarrollo Software y Administración de Sistemas y Bases de Datos
+            desde principios de los años 90. En estos más de 30 años de experiencia, he intervenido
+            en la creación de numerosas soluciones informáticas. Aquí tienes una muestra de algunas
+            de las más relevantes:
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
