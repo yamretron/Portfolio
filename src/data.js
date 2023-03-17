f3a1807be99e487a35a292940ea71b9210e1e0e3
unknown <yamretron@gmail.com>
2023-03-17 11:37:48 +0100

    Despliegue

--- src/data.js
+++ src/data.js
@@ -14,22 +14,6 @@ export const projects = [
     image: "./optraficoweb.gif",
     link: "#",
   },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
 ];
 
 export const testimonials = [
